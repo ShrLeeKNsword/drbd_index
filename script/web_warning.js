79dc293b315c98d66d9232ef1868120f90082f0c
爱走位的KN_sword <83012768+ShrLeeKNsword@users.noreply.github.com>
2022-01-31 22:30:49 +0800

    2022/1/31
    Happy Chinese new year

---
+++ script/web_warning.js
@@ -0,0 +1,16 @@
+export default {
+    data() {
+      return {
+        dialogVisible: false
+      };
+    },
+    methods: {
+      handleClose(done) {
+        this.$confirm('确认关闭？')
+          .then(_ => {
+            done();
+          })
+          .catch(_ => {});
+      }
+    }
+  };
